af030e3a7b1ad515e785d99a51116b66476fc95f
gronteix <gustave.ronteix@hotmail.fr>
2021-02-05 15:29:42 +0100

    improve script

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ set -exE
 
 
 TIMESTAMP=$(date +%s)
-TMP_DIR="/tmp/${TIMESTAMP}"
+TMP_DIR="/tmp/hugo-blog-build-${TIMESTAMP}"
 mkdir "${TMP_DIR}"
 
 hugo 
@@ -14,3 +14,6 @@ git add -A && git commit -m "publishing v${TIMESTAMP}"
 git checkout published
 rm -rf * && cp -r "${TMP_DIR}"/* .
 git add -A && git commit -m "published v${TIMESTAMP}"
+git push
+git checkout master
+rm -rf "${TMP_DIR}"
